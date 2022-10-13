9f07c25e2b7c97d22023df0cbcefc57b5b2979fc
William Fuller <william.fuller@instem.com>
2022-10-13 11:35:41 +0200

    feat: added word counter (not final) and small bug fix with spaces

--- script.js
+++ script.js
@@ -1,35 +1,53 @@
 const textArea = document.getElementById('displayField');
+const wordCountDisplay = document.getElementById('wordCount');
 
-const charPerLine = 100;
+const charPerLine = 125;
 const textAreaHeigth = 4;
 
 let wordList = null;
 let keyPressCount = 0;
+let wordCount = 0;
 
 Array.prototype.random = function () {
     return this[Math.floor((Math.random()*this.length))];
 }
 
 const trackKeyPress = (event) => {
-
+    
     if(event.key === textArea.innerText[keyPressCount]){
+        console.log(event.key);
         textArea.children.item(keyPressCount).className = "correctLetter";
         keyPressCount ++;
+
+        if(event.key === " "){
+            wordCount ++;
+            updateWordCountDisplay();
+        }
     }
+    
     else if(event.key === "Backspace"){
         if(keyPressCount != 0){
             keyPressCount --;
             textArea.children.item(keyPressCount).removeAttribute("class");
 
+            if(textArea.innerText[keyPressCount] === " "){
+                wordCount --;
+                updateWordCountDisplay();
+            }
         }
     }
     else{
         textArea.children.item(keyPressCount).className = "incorrectLetter";
         keyPressCount ++;
     }
+    
+
 
 };
 
+const updateWordCountDisplay = () => {
+    wordCountDisplay.innerText = `${wordCount}`;
+}; 
 const getWordList = async () => {
     const response = await fetch('wordlist.txt');
     const wordListAsString = await response.text();
@@ -37,7 +55,6 @@ const getWordList = async () => {
 }
 
 const fillEntireTextArea = async () => {
-    let newLine = "";
     for (let i = 0; i < textAreaHeigth; i++){
         textArea.innerHTML += await getNewTextAreaRow();
     }
@@ -58,7 +75,6 @@ const getNewTextAreaRow = async () => {
             for(let c of chosenWord){
                 textAreaRowRowValue += `<span>${c}</span>`
             }
-            textAreaRowRowValue += `<span> </span>`
         }
         i += chosenWord.length + 1;
     }
